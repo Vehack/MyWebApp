4a5d919e3107cabf9a3d00e3eb3a39ac3f785520
Vehack <vvehack@mail.ru>
2021-12-16 03:25:46 +0300

    maybe final commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import requests
 from flask import Flask,render_template,request,redirect
 import psycopg2
 
@@ -9,32 +8,27 @@ connection = psycopg2.connect(database='mywebapp',
                                 host='127.0.0.1')
 connection.autocommit = True
 
-
-@app.route('/login', methods=['get'])
-def index():
-    return render_template('login.html')
-
-
+cursor = connection.cursor()
+ex = list()
 @app.route('/login', methods=['post', 'get'])
 def login():
     if request.method == 'POST':
         if request.form.get("login"):
             username = request.form.get('username')
             password = request.form.get('password')
-            attention = 'Ошибка! Поле пустое.'
+            attention = 'Ошибка! Поля пустые.'
 
             if username == '' or password == '':
                 return render_template('login.html', attention = attention)
             else:
                 try:
-                    with connection.cursor() as cursor:
-                        cursor.execute('SELECT * FROM service.users WHERE login=%s AND password=%s',
-                                        (str(username), str(password)))
-                        records = cursor.fetchall()
-                        if len(records) != 0:
-                            return render_template('account.html',full_name = records[0][1],login = username,password=password)
-                        else:
-                            return render_template('login.html', attention=attention)
+                    cursor.execute('SELECT * FROM service.users WHERE login=%s AND password=%s',
+                                    (str(username), str(password)))
+                    records = cursor.fetchall()
+                    if len(records) != 0:
+                        return render_template('account.html',full_name = records[0][1],login = username,password=password)
+                    else:
+                        return render_template('login.html', attention=attention)
                 except Exception as _ex:
                     print(_ex)
                 finally:
@@ -43,6 +37,8 @@ def login():
                         print('Closed')
         elif request.form.get("registration"):
             return redirect("/registration")
+    return render_template('login.html')
+
 
 @app.route('/registration', methods=['POST', 'GET'])
 def registration():
@@ -51,22 +47,21 @@ def registration():
         login = request.form.get('username')
         password = request.form.get('password')
         attention = "Поля пустые!"
-        if login == '' or password == '':
+        attention2 = 'Пользователь с таким логином уже существует!'
+        if name == '' or login == '' or password == '':
             return render_template('registration.html', attention=attention)
         else:
-            try:
-                with connection.cursor() as cursor:
-                    cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
-                                   (str(name), str(login), str(password)))
-            except Exception as _ex:
-                print(_ex)
-            finally:
-                if connection:
-                    connection.close()
-                    print('Closed')
-        return redirect('/login')
-
+            cursor.execute(f"""SELECT * FROM service.users WHERE login='{login}'""")
+            result = cursor.fetchall()
+            if len(result) != 0:
+                return render_template('registration.html', attention2=attention2)
+            else:
+                cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                            (str(name), str(login), str(password)))
+                return redirect('/login')
     return render_template('registration.html')
+
+
 
 
 if __name__ == '__main__':
